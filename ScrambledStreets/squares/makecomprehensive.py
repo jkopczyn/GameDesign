1b20c4623bf4d6a01d85c7c8256985523c72750b
Jacob Kopczynski <ja.kopczynski@gmail.com>
2017-06-18 10:07:06 -0700

    convert to 0-indexing; file now generating successfully

--- ScrambledStreets/squares/makecomprehensive.py
+++ ScrambledStreets/squares/makecomprehensive.py
@@ -10,20 +10,20 @@ def edges_to_type_and_rotation(low,high):
     parities = [low%2, high%2]
     if parities == [1, 0]:
         rotation = -(low - 1)/2 % 4
-        edgetype = edges_to_type(1, (high - (low - 1))%8)
+        edgetype = edges_to_type(0, (high - low)%8)
     elif parities == [0, 1]:
         rotation = -(high - 1)/2 % 4
-        edgetype = edges_to_type(1, (low - (high - 1))%8)
+        edgetype = edges_to_type(0, (low - high)%8)
     else:
         rotation, edgetype = {
-                1: {3: [3, 2], 5: [0, 4], 7: [0, 2]},
-                2: {8: [3, 8], 6: [0, 7], 4: [0, 8]},
-                3: {1: [3, 2], 5: [2, 2], 7: [1, 4]},
-                4: {8: [1, 7], 6: [3, 8], 2: [0, 8]},
-                5: {3: [2, 2], 1: [0, 4], 7: [1, 2]},
-                6: {8: [2, 8], 2: [0, 7], 4: [3, 8]},
-                7: {3: [1, 4], 5: [1, 2], 1: [0, 2]},
-                8: {2: [3, 8], 6: [2, 8], 4: [1, 7]}}[low][high]
+                0: {2: [3, 2], 4: [0, 4], 6: [0, 2]},
+                1: {7: [3, 8], 5: [0, 7], 3: [0, 8]},
+                2: {0: [3, 2], 4: [2, 2], 6: [1, 4]},
+                3: {7: [1, 7], 5: [3, 8], 1: [0, 8]},
+                4: {2: [2, 2], 0: [0, 4], 6: [1, 2]},
+                5: {7: [2, 8], 1: [0, 7], 3: [3, 8]},
+                6: {2: [1, 4], 4: [1, 2], 0: [0, 2]},
+                7: {1: [3, 8], 5: [2, 8], 3: [1, 7]}}[low][high]
     return [rotation, edgetype]
 
 
@@ -31,23 +31,29 @@ def edges_to_type(low, high):
     if low > high:
         low, high = high, low
     lookup = {
-            2: {4: '8', 6: '7'}, 
-            1: {8: '1', 7: '2', 6: '3', 5: '4', 4: '5', 0: '1'}}
+            1: {3: '8', 5: '7'},
+            0: {7: '1', 6: '2', 5: '3', 4: '4', 3: '5', 1: '6'}}
     return lookup[low][high]
 
 COLOR_ENCODING = [0,1,2]
 
 def make_line(pairs, colors):
-    charblobs = imap(str,izip(imap(edges_to_type_and_rotation, pairs),colors))
-    return ' '.join(c for c in ''.join(charblobs) if c in DIGITS)
+    pairwise_typing = lambda x: edges_to_type_and_rotation(*x)
+    charblobs = imap(str,izip(imap(pairwise_typing, pairs),colors))
+    return ' '.join(c for c in ''.join(charblobs) if c in DIGITS)+'\n'
+
+def pair_up_list(l):
+    return list(izip(*[iter(l)]*2))
 
 def all_possible_shape_combinations():
-    point_orderings = permutations(range(1,9), 6)
+    point_orderings = permutations(range(8), 6)
+    y = list(imap(pair_up_list,point_orderings))
+    print y[:3]
     pairs_orderings = filter(
             lambda x: sorted(x)==x,
-            izip(*[iter(point_orderings)]*2)
+            y
             )
-    return [make_line(pairs, colors) for order in pairs_orderings for
+    return [make_line(pairs, colors) for pairs in pairs_orderings for
             colors in permutations(COLOR_ENCODING)]
 
 
